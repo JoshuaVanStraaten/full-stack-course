9b9edb7b218fb087bef3656343d63e2669dc95fc
Joshua Van Straaten <joshua.vanstraaten@corigine.com>
2025-07-28 10:00:28 +0200

    Blog web app: Add Update, Delete and View functionality
    Add support for updating, deleting and viewing full posts.
    
    Signed-off-by: Joshua Van Straaten <joshuavanstraaten100@gmail.com>

--- 5.0 Capstone Project 3 - Online Blog/index.js
+++ 5.0 Capstone Project 3 - Online Blog/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { v4 as uuidv4 } from 'uuid';
 
 // Server setup
 const app = express();
@@ -31,9 +32,65 @@ app.post("/create", (req, res) => {
     // Create a timestamp for the post
     const timestamp = new Date().toLocaleString();
 
+    // Generate a unique ID
+    const id = uuidv4();
+
     // Update in-app DB
-    blogPosts.push({title: title, author: author, content: content, date: timestamp});
+    blogPosts.push({id: id, title: title, author: author, content: content, date: timestamp});
     res.render("index.ejs", {blogPosts: blogPosts});
+});
+
+// Handle editing a post
+app.get("/edit/:id", (req, res) => {
+    const id = req.params.id;
+    // Get post using UUID
+    const post = blogPosts.find(p => p.id === id);
+
+    if (!post) {
+        return res.status(404).send("Post not found");
+    }
+
+    res.render("edit.ejs", { post });
+});
+
+app.post("/edit/:id", (req, res) => {
+    const id = req.params.id;
+    const {title, author, content} = req.body;
+    const index = blogPosts.findIndex(p => p.id === id);
+
+    if (index === -1) {
+        return res.status(404).send("Post not found");
+    }
+
+    // Update blog within Array
+    blogPosts[index] = {
+        id: id,
+        title: title,
+        author: author,
+        content: content,
+        date: new Date().toLocaleString()
+    };
+
+    res.render("index.ejs", {blogPosts: blogPosts});
+})
+
+// Handle deleting a post
+app.post("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    // Filter out Post
+    blogPosts = blogPosts.filter(p => p.id !== id);
+
+    res.render("index.ejs", {blogPosts: blogPosts});
+});
+
+// Handle viewing a post (in full)
+app.get("/view/:id", (req, res) => {
+    const id = req.params.id;
+    const index = blogPosts.findIndex(p => p.id === id);
+    // Get Post
+    const post = blogPosts[index];
+
+    res.render("view_post.ejs", {post: post})
 });
 
 // Start the server
